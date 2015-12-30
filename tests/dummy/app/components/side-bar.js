049300034267ef0cd8c68746d088a63143ed39a5
Keith Broughton <keithbro256@gmail.com>
2015-12-30 19:09:40 +0000

    Add unit test

---
+++ tests/dummy/app/components/side-bar.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import FitToWindow from 'ember-fit-to-window-mixin/mixins/fit-to-window';
+
+export default Ember.Component.extend(FitToWindow, {
+  classNames: ['side-bar'],
+});
--- tests/unit/mixins/fit-to-window-test.js
+++ tests/unit/mixins/fit-to-window-test.js
@@ -1,12 +1,55 @@
 import Ember from 'ember';
-import FitToWindowMixin from '../../../mixins/fit-to-window';
+import FitToWindowMixin from 'ember-fit-to-window-mixin/mixins/fit-to-window';
 import { module, test } from 'qunit';
 
 module('Unit | Mixin | fit to window');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let FitToWindowObject = Ember.Object.extend(FitToWindowMixin);
-  let subject = FitToWindowObject.create();
-  assert.ok(subject);
+test('component lifecycle', function(assert) {
+  let newOuterHeight;
+  let attachedResizeHandler = false;
+  let detachedResizeHandler = false;
+
+  const component = Ember.Component.extend(FitToWindowMixin).create({
+    ignoreHeight: 100,
+
+    $(selector) {
+      return {
+        height() {
+          if (selector === window) { return 500; }
+        },
+
+        outerHeight(_newOuterHeight) {
+          if (selector === undefined) { newOuterHeight = _newOuterHeight; }
+        },
+
+        on(event, func) {
+          if (event === 'resize') { attachedResizeHandler = func; }
+        },
+
+        off(event, func) {
+          if (event === 'resize') { detachedResizeHandler = func; }
+        },
+      };
+    },
+  });
+
+  assert.strictEqual(
+    newOuterHeight, undefined,
+    "component's outerHeight not yet set"
+  );
+  assert.notOk(attachedResizeHandler, "resize handler not yet attached");
+  assert.notOk(detachedResizeHandler, "resize handler not yet detached");
+
+  component.didRender();
+
+  assert.strictEqual(
+    newOuterHeight, 400,
+    "component's outerHeight has been set"
+  );
+  assert.ok(attachedResizeHandler, "resize handler has been attached");
+  assert.notOk(detachedResizeHandler, "resize handler not yet detached");
+
+  component.willDestroy();
+
+  assert.ok(detachedResizeHandler, "resize handler has been detached");
 });
